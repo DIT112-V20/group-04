1adbdedc2e52ec45b484fb065ebbee06d1d80ee9
samgun-6 <samuel.gunnarsson@gmail.com>
2020-04-09 17:53:51 +0200

    Add webserver to smartcar
    Add webserver for the ESP32 microcontroller and implement basic client requests for start and stop in order to handle client commands
    
    Issues: #9
    
    Contributors:
    @SandraSmolerEisenberg
    @samgun-6
    @Enilo
    @krasen86
    @chrytsa

--- arduino/healthRover/healthRover.ino
+++ arduino/healthRover/healthRover.ino
@@ -7,16 +7,21 @@ const int START_SPEED = 50;
 const int MIN_OBSTACLE_DISTANCE = 300;
 const int BAUD_RATE = 115200;
 const int STOP = 0;
-const char* SSID = "";
-const char* PASSWORD = "";
+const char* SSID = "ssid_goes_here";
+const char* PASSWORD = "password_goes_here";
 int frontSensorReading;
+
+String clientRequest;
+WiFiServer server(80);
+WiFiClient client;
+String result;
 
 BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
 SimpleCar car(control);
 VL53L0X sensor;
-//Static Local IP configuration for identifying car.
+// Static Local IP configuration for identifying car.
 IPAddress local_IP(192, 168, 1, 200);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
@@ -25,8 +30,10 @@ void setup(){
   Serial.begin(BAUD_RATE);
   delay(10);
 
+  clientRequest = "";
   Wire.begin();
   connectToWiFi();
+  server.begin();
 
   // Allow half a second to connect the sensor
   sensor.setTimeout(500);
@@ -39,27 +46,47 @@ void setup(){
   sensor.startContinuous();
 }
 void loop() {
-
   frontSensorReading = sensor.readRangeContinuousMillimeters();
   // Stop when distance is less than MIN_OBSTACLE_DISTANCE and
   // disregard 0 reading because its a null reading from the sensor
   if (frontSensorReading <= MIN_OBSTACLE_DISTANCE && frontSensorReading > 0){
     car.setSpeed(STOP);
   }
-  //Stopping the car if the wi-fi connection is lost.
+  // Stopping the car if the wi-fi connection is lost.
   if (WiFi.status() != WL_CONNECTED){
     car.setSpeed(STOP);
     connectToWiFi();
   }
+    // Handle client requests
+    client = server.available();
+    if (!client) { return; }
+    while(!client.available()){  delay(1); }
+    clientRequest = (readIncomingRequest());
+    // Removing the first five and last nine characters of the clients GET_REQUEST
+    clientRequest.remove(0, 5);
+    clientRequest.remove(clientRequest.length()-9,9);
+
+    if (clientRequest == "stop") {
+      handle_stop();
+      delay(1);
+    }
+
+    if (clientRequest == "forward") {
+      handle_forward();
+      delay(1);
+    }
+    client.flush();
+
+
 }
 void connectToWiFi(){
-  //Configures and checks if the configuration is successful.
+  // Configures and checks if the configuration is successful.
   if (!WiFi.config(local_IP, gateway, subnet)) {
     Serial.println("STA Failed to configure");
   } else{
     WiFi.begin(SSID, PASSWORD);
   }
-    //wait for wifi to connect
+    // wait for wifi to connect
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.println("Connecting to WiFi...");
@@ -67,3 +94,22 @@ void connectToWiFi(){
   Serial.println("Connected to the WiFi network");
   Serial.println(WiFi.localIP());
 }
+
+String readIncomingRequest(){
+  while(client.available()) {
+    clientRequest = (client.readStringUntil('\r'));
+    if ((clientRequest.indexOf("HTTP/1.1")>0)&&(clientRequest.indexOf("/favicon.ico")<0)){
+    result = clientRequest;
+    }
+  }
+  return result;
+}
+
+void handle_stop() {
+  car.setSpeed(STOP);
+}
+
+void handle_forward() {
+  car.setSpeed(START_SPEED);
+}
+
