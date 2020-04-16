d8369a228f36c0ddfb48f8532f424a4c733b0656
eemilj <gusjesee@student.gu.se>
2020-04-16 16:43:54 +0300

    Change HTTP request implementation

--- app/src/main/java/se/healthrover/entities/HealthRoverCar.java
+++ app/src/main/java/se/healthrover/entities/HealthRoverCar.java
@@ -2,9 +2,9 @@ package se.healthrover.entities;
 
 public enum HealthRoverCar {
     //A list of cars used since a database is not used
-    HEALTH_ROVER_CAR1("http://www.google.com", "CAR1"),
-    HEALTH_ROVER_CAR2("http://192.168.1.201/", "CAR2"),
-    HEALTH_ROVER_CAR3("http://192.168.1.202/", "CAR3");
+    HEALTH_ROVER_CAR1("http://192.168.1.200/request?type=", "HealthRover"),
+    HEALTH_ROVER_CAR2("http://192.168.1.201/request?type=", "CAR2"),
+    HEALTH_ROVER_CAR3("http://192.168.1.202/request?type=", "CAR3");
 
     private final String url;
     private final String carName;
--- arduino/healthRover/healthRover.ino
+++ arduino/healthRover/healthRover.ino
@@ -2,25 +2,25 @@
 #include <Wire.h>
 #include <VL53L0X.h>
 #include <WiFi.h>
+#include <WebServer.h>
 
 const int START_SPEED = 50;
 const int MIN_OBSTACLE_DISTANCE = 300;
 const int BAUD_RATE = 115200;
 const int STOP = 0;
-const char* SSID = "ssid_goes_here";
-const char* PASSWORD = "password_goes_here";
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+
 int frontSensorReading;
 
-String clientRequest;
-WiFiServer server(80);
-WiFiClient client;
-String result;
+WebServer server(80);
 
 BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
 SimpleCar car(control);
 VL53L0X sensor;
+
 // Static Local IP configuration for identifying car.
 IPAddress local_IP(192, 168, 1, 200);
 IPAddress gateway(192, 168, 1, 1);
@@ -29,11 +29,8 @@ IPAddress subnet(255, 255, 255, 0);
 void setup(){
   Serial.begin(BAUD_RATE);
   delay(10);
-
-  clientRequest = "";
   Wire.begin();
   connectToWiFi();
-  server.begin();
 
   // Allow half a second to connect the sensor
   sensor.setTimeout(500);
@@ -41,60 +38,36 @@ void setup(){
     Serial.println("Failed to detect and initialize sensor!");
     while (1) {}
   }
+
   // Start continuous back-to-back mode
   // (take readings as fast as possible).
   sensor.startContinuous();
+
+  // Car URL to handle the request
+  server.on("/request", HTTP_GET, handleRequest);
+
+  server.begin();
 }
 void loop() {
+  server.handleClient();
+
   frontSensorReading = sensor.readRangeContinuousMillimeters();
   // Stop when distance is less than MIN_OBSTACLE_DISTANCE and
   // disregard 0 reading because its a null reading from the sensor
   if (frontSensorReading <= MIN_OBSTACLE_DISTANCE && frontSensorReading > 0){
     car.setSpeed(STOP);
-  }
-  // Stopping the car if the wi-fi connection is lost.
-  if (WiFi.status() != WL_CONNECTED){
+  }else if (WiFi.status() != WL_CONNECTED){
     car.setSpeed(STOP);
     connectToWiFi();
   }
-    // Handle client requests
-    client = server.available();
-    if (!client) { return; }
-    while(!client.available()){  delay(1); }
-    clientRequest = (readIncomingRequest());
-    // Removing the first five and last nine characters of the clients GET_REQUEST
-    clientRequest.remove(0, 5);
-    clientRequest.remove(clientRequest.length()-9,9);
-
-    if (clientRequest == "stop") {
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-type:text/html");
-      client.println("Connection: close");
-      client.println();
-      handle_stop();
-      delay(1);
-    }
-
-    if (clientRequest == "forward") {
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-type:text/html");
-      client.println("Connection: close");
-      client.println();
-      handle_forward();
-      delay(1);
-    }
-    client.flush();
-
-
 }
+
 void connectToWiFi(){
-  // Configures and checks if the configuration is successful.
   if (!WiFi.config(local_IP, gateway, subnet)) {
     Serial.println("STA Failed to configure");
-  } else{
-    WiFi.begin(SSID, PASSWORD);
   }
-    // wait for wifi to connect
+  WiFi.begin(ssid, password);
+  //wait for wifi to connect
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.println("Connecting to WiFi...");
@@ -103,21 +76,31 @@ void connectToWiFi(){
   Serial.println(WiFi.localIP());
 }
 
-String readIncomingRequest(){
-  while(client.available()) {
-    clientRequest = (client.readStringUntil('\r'));
-    if ((clientRequest.indexOf("HTTP/1.1")>0)&&(clientRequest.indexOf("/favicon.ico")<0)){
-    result = clientRequest;
-    }
-  }
-  return result;
-}
-
-void handle_stop() {
-  car.setSpeed(STOP);
-}
-
-void handle_forward() {
+void moveForward() {
   car.setSpeed(START_SPEED);
 }
 
+void stopCar() {
+  car.setSpeed(STOP);
+}
+void handleRequest() {
+  if (!server.hasArg("type")) {
+    server.send(404);
+    return;
+  }
+  String appRequest = server.arg("type");
+  if (appRequest.equals("forward")) {
+    moveForward();
+    server.send(200);
+  }
+  else if (appRequest.equals("stop")) {
+    stopCar();
+    server.send(200);
+  }
+  else  if (appRequest.equals("status")) {
+    server.send(200);
+  }
+  else {
+    server.send(404);
+  }
+}
